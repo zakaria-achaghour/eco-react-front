350013928e3cae2e02b42896e15b0851b2dfe627
Zakaria Achaghour <achaghourzakaria@gmail.com>
2022-05-13 22:58:26 +0100

    WIP: Shop page Finish

--- src/core/ApiCore.js
+++ src/core/ApiCore.js
@@ -1,4 +1,5 @@
 import { API_URL } from "../config"
+import { notify } from "../helpers/Toast"
 
 export const getProducts = (sortBy, order, limit) =>{
    return fetch(`${API_URL}/products?sortBy=${sortBy}&order=${order}&limit=${limit}`)
@@ -6,3 +7,37 @@ export const getProducts = (sortBy, order, limit) =>{
     .then(res =>res.products)
     .catch(err => console.log(err))
 }
+
+export const getCategories = () => {
+  return  fetch(`${API_URL}/categories`)
+    .then(res =>res.json() )
+   .then(res =>res.categories)
+   .catch(error =>
+       notify('error','Internal Server Error')
+   )
+}
+
+export const filterProducts = (skip, limit, filters) => {
+    console.log(filters)
+    const data = {
+        skip,
+        limit,
+      filters
+    }
+    return  fetch(`${API_URL}/products/search`,{
+        method: "POST",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    })
+      .then(res =>res.json() )
+     .then(res =>res.products)
+
+     .catch(error =>{
+         console.log(error)
+         notify('error','Internal Server Error')
+        }
+     )
+  }
--- src/core/ShowImage.js
+++ src/core/ShowImage.js
@@ -4,7 +4,7 @@ import { API_URL } from '../config'
 const ShowImage = ({item, url,className}) => {
   return (
     <div>
-        <img className={className} src={`${API_URL}/${url}/${item._id}`} alt={`${item.name}`} />
+        <img  className={className} src={`${API_URL}/${url}/${item._id}`} alt={`${item.name}`} />
     </div>
   )
 }
